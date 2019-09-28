564084ea3a8b9eb48e3138c9a1d7652906e414a8
evon16888 <evon16888@gmail.com>
2019-09-29 07:14:06 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,11 @@ export PATH
 
 #安装相关依赖
 sudo apt-get update
-sudo apt-get install git pkg-config
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
-sudo apt-get install libboost-all-dev
-sudo apt-get install software-properties-common
+sudo apt-get install -y git pkg-config
+sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+sudo apt-get install -y libboost-all-dev
+sudo apt-get install -y software-properties-common
 sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
 sudo apt-get install libdb4.8-dev libdb4.8++-dev
 
 #下载源码包
