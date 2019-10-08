15b323cfe7845b56024db7d5ee63f4263d7384a7
evon16888 <evon16888@gmail.com>
2019-10-08 23:06:33 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,30 +2,28 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-#安装相关依赖
-sudo apt-get update
-sudo apt-get install -y git pkg-config
-sudo apt-get install -y build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install -y software-properties-common
-sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get install libdb4.8-dev libdb4.8++-dev
+# 安装wget
+yum -y install wget
 
-#下载源码包
-git clone https://github.com/OmniLayer/omnicore.git
-cd omnicore/
-./autogen.sh
-./configure & make
-make install
+# 下载USDT安装包
+wget https://bintray.com/artifact/download/omni/OmniBinaries/omnicore-0.5.0-x86_64-linux-gnu.tar.gz
 
-#创建基本配置
-read -p "请输入安装目录[默认/home]： " upath
-if [[ -z "${uport}" ]];then
-	upath="/home"
-fi
-mkdir -p "${upath}"/usdt/data
-touch "${upath}"/usdt/usdt.conf
-cat>>"${upath}"/usdt/usdt.conf<<EOF
+# 解包
+tar -zvxf omnicore-0.5.0-x86_64-linux-gnu.tar.gz
+
+# 安装在bin目录下
+install -m 0755 -o root -g root -t /usr/local/bin ./omnicore-0.5.0/bin/*
+
+# 在根目录创建配置文件目录
+mkdir ~/.bitcoin
+
+# 创建目录
+mkdir /www/coin/usdt_data
+
+# 创建配置文件
+touch ~/.bitcoin/bitcoin.conf
+# 写入文件
+cat>>~/.bitcoin/bitcoin.conf<<EOF
 server=1  
 txindex=1 
 rpcuser=rpcuser
@@ -39,10 +37,8 @@ logtimestamps=1
 omnidebug=tally  
 omnidebug=packets
 omnidebug=pending
+datadir=/www/coin/usdt_data
 EOF
 
-#启动程序
-/usr/local/bin/omnicored -conf="${upath}"/usdt/usdt.conf -datadir="${upath}"/usdt/data &
-
-#加入开机启动
-echo "/usr/local/bin/omnicored -conf="${upath}"/usdt/usdt.conf -datadir="${upath}"/usdt/data  &" >> /etc/rc.local
+# 启动USDT
+/usr/local/bin/omnicored &
